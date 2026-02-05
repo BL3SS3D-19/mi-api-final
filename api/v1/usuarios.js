2fb628e7e207307cfac6f9e61ef1ff13a81e9cd6
BL3SS3D-19 <peibol1912@gmail.com>
2026-02-05 20:04:37 +0100

    feat: add new api endpoints

--- api/v1/usuarios.js
+++ api/v1/usuarios.js
@@ -6,7 +6,7 @@ export default async function getUsuarios(req, res) {
         let usuarios = await User.find().select('-password -_id'); //quitamos el psswd por seguridad;
         res.status(200).json({ usuarios });
     } catch (error) {
-        res.status(500).json({ message: 'Error al obtener los usuarios' })
+        res.status(500).json({ message: 'Error al obtener los usuarios' + error.message })
     }
 
 }
